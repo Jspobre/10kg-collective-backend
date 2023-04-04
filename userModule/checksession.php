1b66581ab939b8e63c373d53115ae90293ced2d5
Jack Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2023-04-04 23:03:15 +0800

    added array on checksession

--- userModule/checksession.php
+++ userModule/checksession.php
@@ -2,7 +2,14 @@
     include_once "../db_conn.php";
 
 if(isset($_SESSION['user_id'])){
-
+    $response = array('response_status' => 1
+    , 'user_id' => $user['user_id']
+    , 'user_type' => $_SESSION['user_type']
+    , 'full_name' => $user['full_name']
+    , 'email_address' => $user['email_address']
+    , 'contact_no' => $user['contact_no']
+    , 'address' => $user['address']
+);
 
 
 }else {
