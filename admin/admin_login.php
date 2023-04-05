6dbd1af95f8b367ad4dc557f66c8fdd8606519c1
Jack Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2023-04-05 13:58:02 +0800

    added admin folder

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -7,17 +7,26 @@ include_once "../db_conn.php";
         $admin_pass = $_POST['admin_password'];
 
         $result= mysqli_query($conn,"SELECT * FROM admin WHERE admin_email = '$admin_email' AND admin_password = '$admin_pass'");
-
         if(mysqli_num_rows($result) > 0){ // Use mysqli_num_rows instead of mysqli_fetch_assoc
             $row = mysqli_fetch_assoc($result); // Fetch the row only once and store it in $row
             if($admin_pass == $row['admin_password']){
-            echo "uy gagi";
+
+                $_SESSION['admin_id'] = $row['admin_id']; //set session
+                    $_SESSION['user_type'] = 'A';
+
+                    $response = array('response_status' => 1
+                                    , 'admin_id' => $row['admin_id']
+                                    , 'user_type' => $_SESSION['user_type']
+                                    , 'admin_email' => $row['admin_email']                             
+                                );
+
+                    // send as json object to react
+                    echo json_encode($response);
         }
         } else {
               echo 2;
 
         }
-
-
+              
 
     }
