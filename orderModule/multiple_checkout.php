d2ccdcc63ed313ef719f54dfdf2d103be7c612c3
Jack Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2023-04-26 00:05:15 +0800

    added tentative multiple checkout file

--- orderModule/multiple_checkout.php
+++ orderModule/multiple_checkout.php
@@ -1,5 +1,24 @@
 <?php
 
+include_once "../db_conn.php";
 
+if(isset($_POST['user_id'])){
+    $user_id = $_POST['user_id'];
+    $orders = json_decode($_POST['orders'], true); 
 
+    foreach($orders as $order){
+        $order_id = $order['order_id']; 
+        $order_status = 'P'; 
+
+        // Update the order_status
+        $query = "UPDATE orders SET order_status = ? WHERE order_id = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('si', $order_status, $order_id);
+        if($stmt->execute()){
+                echo 1;
+                $stmt->close();
+        }
+     
+    }
+}
 ?>
