90f1463c22394b0a7e7eaebaaab8ebd50224b25a
unknown <jackvincentsanchez.pobre@bicol-u.edu.ph>
2023-05-12 00:46:10 +0800

    added hash password

--- userModule/update_password.php
+++ userModule/update_password.php
@@ -5,12 +5,12 @@ include_once '../db_conn.php';
 if(isset($_POST['user_id'])){
     $user_id = $_POST['user_id'];
     $current_pass = $_POST['current_pass'];
-    $new_pass = $_POST['new_pass'];
+    $new_pass = password_hash($_POST['new_pass'], PASSWORD_BCRYPT);
     
       $result = query($conn, "SELECT * FROM user WHERE user_id = $user_id");
 
 
-    if($result[0]['password'] == $current_pass){
+    if(password_verify( $current_pass, $result[0]['password'] )){
         $stmt = $conn->prepare("UPDATE user SET password = ?");
         $stmt->bind_param("s", $new_pass);
         if($stmt->execute()){
@@ -21,8 +21,7 @@ if(isset($_POST['user_id'])){
     }else {
         echo 3;
     }
-    // print_r($result);
-    // echo $result[0]["password"];
+  
 }
 
 ?>
