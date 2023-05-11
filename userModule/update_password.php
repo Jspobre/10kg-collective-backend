f96723fd45e7bf71e455a0319b1905b058fe9099
unknown <jackvincentsanchez.pobre@bicol-u.edu.ph>
2023-05-12 01:23:54 +0800

    fixed update password

--- userModule/update_password.php
+++ userModule/update_password.php
@@ -2,26 +2,32 @@
 
 include_once '../db_conn.php';
 
-if(isset($_POST['user_id'])){
+if(isset($_POST['user_id'])) {
     $user_id = $_POST['user_id'];
     $current_pass = $_POST['current_pass'];
     $new_pass = password_hash($_POST['new_pass'], PASSWORD_BCRYPT);
-    
-      $result = query($conn, "SELECT * FROM user WHERE user_id = $user_id");
 
+    $stmt = $conn->prepare("SELECT password FROM user WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    if(password_verify( $current_pass, $result[0]['password'] )){
-        $stmt = $conn->prepare("UPDATE user SET password = ?");
-        $stmt->bind_param("s", $new_pass);
-        if($stmt->execute()){
-            echo 1;
-        }else {
-            echo 2;
+        if($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+
+        if(password_verify($current_pass, $row['password'])) {
+            $stmt = $conn->prepare("UPDATE user SET password = ? WHERE user_id = ?");
+            $stmt->bind_param("si", $new_pass, $user_id);
+
+            if($stmt->execute()) {
+                echo 1;
+            } else {
+                echo 2;
+            }
+        } else {
+            echo 3;
         }
-    }else {
-        echo 3;
-    }
-  
+    } 
 }
 
 ?>
